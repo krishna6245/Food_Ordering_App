09f5885dc018c0f1dc8ed85659b7240e5a26b7fc
krishna6245 <125036779+krishna6245@users.noreply.github.com>
2024-05-11 03:06:35 +0530

    Modified the Search Fragment according to new Model class

--- app/src/main/java/com/example/foodorderingapp/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/fragment/SearchFragment.kt
@@ -1,6 +1,7 @@
 package com.example.foodorderingapp.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,19 +10,25 @@ import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodorderingapp.R
 import com.example.foodorderingapp.adapters.MenuItemAdapter
+import com.example.foodorderingapp.dataModels.MenuItemModel
 import com.example.foodorderingapp.databinding.FragmentSearchBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class SearchFragment : Fragment() {
 
     private lateinit var binding : FragmentSearchBinding
     private lateinit var searchItemAdapter: MenuItemAdapter
-    private lateinit var searchItemFoodNames : MutableList<String>
-    private lateinit var searchItemFoodImages : MutableList<Int>
-    private lateinit var searchItemFoodPrices : MutableList<Int>
 
-    private lateinit var filteredFoodItemNames : MutableList<String>
-    private lateinit var filteredFoodItemImages : MutableList<Int>
-    private lateinit var filteredFoodItemPrices : MutableList<Int>
+    private lateinit var auth : FirebaseAuth
+    private lateinit var database: FirebaseDatabase
+
+    private lateinit var searchMenuList : MutableList<MenuItemModel>
+    private lateinit var filteredMenuList : MutableList<MenuItemModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -36,28 +43,41 @@ class SearchFragment : Fragment() {
     }
 
     private fun init(){
-        setLists()
-        setAdapters()
         setListeners()
+        initializeUiElements()
     }
-    private fun setLists(){
-        searchItemFoodNames = mutableListOf("Biryani","Burger","Pizza","Momos","Rolls","Fries","Sandwich","Muffins")
-        searchItemFoodPrices = mutableListOf(100,70,150,60,75,80,40,60)
-        val a:Int = R.drawable.dummy_image
-        val b:Int = R.drawable.dummy_image_1
-        searchItemFoodImages = mutableListOf(a,b,a,b,a,b,a,b)
+    private fun initializeUiElements(){
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
 
-        filteredFoodItemNames = mutableListOf()
-        filteredFoodItemImages = mutableListOf()
-        filteredFoodItemPrices = mutableListOf()
+        searchMenuList = mutableListOf()
+        filteredMenuList = mutableListOf()
 
-        filteredFoodItemNames.addAll(searchItemFoodNames)
-        filteredFoodItemImages.addAll(searchItemFoodImages)
-        filteredFoodItemPrices.addAll(searchItemFoodPrices)
+        retrieveMenu()
+    }
+    private fun retrieveMenu(){
+        val menuReference = database.reference.child("menu")
 
+        menuReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                searchMenuList.clear()
+
+                for (menuItemSnapshot in snapshot.children){
+                    val menuItem = menuItemSnapshot.getValue(MenuItemModel::class.java)
+                    menuItem?.let {
+                        searchMenuList.add(menuItem)
+                    }
+                }
+                filteredMenuList.addAll(searchMenuList)
+                setAdapters()
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("Error","Database Error in ViewMenuActivity:- ${error.toString()}")
+            }
+        })
     }
     private fun setAdapters(){
-        searchItemAdapter = MenuItemAdapter(filteredFoodItemNames,filteredFoodItemImages,filteredFoodItemPrices,requireActivity())
+        searchItemAdapter = MenuItemAdapter(requireContext(), filteredMenuList)
         binding.menuFragmentMenuItemList.layoutManager = LinearLayoutManager(requireContext())
         binding.menuFragmentMenuItemList.adapter = searchItemAdapter
     }
@@ -77,15 +97,10 @@ class SearchFragment : Fragment() {
 
     }
     private fun filterMenuItems(query : String){
-        filteredFoodItemNames.clear()
-        filteredFoodItemImages.clear()
-        filteredFoodItemPrices.clear()
-
-        searchItemFoodNames.forEachIndexed { index, foodName ->
-            if(foodName.contains(query,ignoreCase = true)){
-                filteredFoodItemNames.add(searchItemFoodNames[index])
-                filteredFoodItemImages.add(searchItemFoodImages[index])
-                filteredFoodItemPrices.add(searchItemFoodPrices[index])
+        filteredMenuList.clear()
+        searchMenuList.forEachIndexed { index, menuItem ->
+            if(menuItem.foodName?.contains(query,ignoreCase = true) == true){
+                filteredMenuList.add(searchMenuList[index])
             }
         }
         searchItemAdapter.notifyDataSetChanged()
